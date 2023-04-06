09acf015634085b6f7816fbcdd0091980e49217e
Clayton Priestley <clayton.priestley@gmail.com>
2023-04-06 11:00:30 -0500

    add standard boilerplate - add spring starter dependencies - create package structure - add empty home page

---
+++ src/main/java/com/idocrew/weddingwise/WeddingWiseApplication.java
@@ -0,0 +1,13 @@
+package com.idocrew.weddingwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingWiseApplication.class, args);
+	}
+
+}
