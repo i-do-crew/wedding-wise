00aff8006c4f674c0cae378bba22da71f9103946
Clayton Priestley <clayton.priestley@gmail.com>
2023-04-12 12:35:23 -0500

    Refactor MusicGenre - renamed to MusicVendor - added join on foreign key vendor_id

--- src/main/java/com/idocrew/weddingwise/entity/MusicVendor.java
+++ src/main/java/com/idocrew/weddingwise/entity/MusicVendor.java
@@ -1,9 +1,10 @@
 package com.idocrew.weddingwise.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
-
-import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
