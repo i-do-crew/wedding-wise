1c7e2f4a4ba219d411304ce89fbb9bb9ab11d2db
Clayton Priestley <clayton.priestley@gmail.com>
2023-04-13 13:16:36 -0500

    Revise User to Group Relationship - remove role_id - revised to Many-to-Many - revise UserRegistrationService to accomodate more than 1 Group - UserWithRoles.grantedAuthorities() to assign roles based upon user groups

--- src/main/java/com/idocrew/weddingwise/entity/UserWithRoles.java
+++ src/main/java/com/idocrew/weddingwise/entity/UserWithRoles.java
@@ -1,10 +1,13 @@
 package com.idocrew.weddingwise.entity;
 
+import org.apache.tomcat.util.buf.StringUtils;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class UserWithRoles extends User implements UserDetails {
 
@@ -14,8 +17,14 @@ public class UserWithRoles extends User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        String roles = "";
-        return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLES_USER");
+        List<Group> groups = this.getUserGroups();
+        List<String> list = new ArrayList<>();
+        list.add("ROLE_USER");
+        for (Group group : groups) {
+            list.add("ROLE_" + group.getCode());
+        }
+        String roles = StringUtils.join(list, ',');
+        return AuthorityUtils.commaSeparatedStringToAuthorityList(roles);
     }
 
     @Override
