599b217e46b898f4722e896adc11e58f2ca76ea2
Clayton Priestley <clayton.priestley@gmail.com>
2023-04-20 10:40:09 -0500

    remove unused import

--- src/main/java/com/idocrew/weddingwise/services/UserRegistrationService.java
+++ src/main/java/com/idocrew/weddingwise/services/UserRegistrationService.java
@@ -1,7 +1,6 @@
 package com.idocrew.weddingwise.services;
 
 import com.idocrew.weddingwise.entity.Customer;
-import com.idocrew.weddingwise.entity.Vendor;
 import com.idocrew.weddingwise.entity.VendorComposite;
 
 public interface UserRegistrationService {
