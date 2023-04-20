6b1d767489a6917b47520eef844de58375fc389b
Andrea Varnado <andrea.h.varnado@gmail.com>
2023-04-20 10:44:34 -0500

    restyled the customer sidebar

--- src/main/java/com/idocrew/weddingwise/repositories/CustomerRepository.java
+++ src/main/java/com/idocrew/weddingwise/repositories/CustomerRepository.java
@@ -1,8 +1,10 @@
 package com.idocrew.weddingwise.repositories;
 
 import com.idocrew.weddingwise.entity.Customer;
+import com.idocrew.weddingwise.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Customer findById(long id);
+    Customer findCustomerByUser(User user);
 }
--- src/main/java/com/idocrew/weddingwise/repositories/VendorRepository.java
+++ src/main/java/com/idocrew/weddingwise/repositories/VendorRepository.java
@@ -1,5 +1,6 @@
 package com.idocrew.weddingwise.repositories;
 
+import com.idocrew.weddingwise.entity.User;
 import com.idocrew.weddingwise.entity.VendorCategory;
 import com.idocrew.weddingwise.entity.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,6 @@ import java.util.List;
 public interface VendorRepository extends JpaRepository<Vendor, Long> {
     Vendor findById(long id);
     Vendor findByBusinessName(String businessName);
+    Vendor findVendorByUser(User user);
     List<Vendor> findByVendorCategory(VendorCategory vendorCategory);
 }
