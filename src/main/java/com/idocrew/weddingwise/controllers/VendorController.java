72484a3b3d943b39bb5b48dab57d2af74ae83fcc
Billie <billie.dorries28@gmail.com>
2023-04-25 14:30:52 -0500

    Refactor services and controllers. corrected eachVendor page

--- src/main/java/com/idocrew/weddingwise/controllers/VendorController.java
+++ src/main/java/com/idocrew/weddingwise/controllers/VendorController.java
@@ -2,9 +2,13 @@ package com.idocrew.weddingwise.controllers;
 
 import com.idocrew.weddingwise.entity.User;
 import com.idocrew.weddingwise.entity.Vendor;
+import com.idocrew.weddingwise.entity.VendorCategory;
 import com.idocrew.weddingwise.repositories.UserRepository;
 import com.idocrew.weddingwise.repositories.VendorCategoryRepository;
 import com.idocrew.weddingwise.repositories.VendorRepository;
+import com.idocrew.weddingwise.services.UserService;
+import com.idocrew.weddingwise.services.VendorCategoryService;
+import com.idocrew.weddingwise.services.VendorUtility;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
@@ -13,22 +17,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
 @Controller
 @RequiredArgsConstructor
 public class VendorController {
 
-    private final VendorRepository vendorRepository;
-    private final VendorCategoryRepository vendorCategoryRepository;
     private final UserRepository userRepository;
     private final VendorUtility vendorUtility;
     private final VendorCategoryService vendorCategoryService;
     private final UserService userService;
 
     private void refactorThisMethod(@CurrentSecurityContext(expression = "authentication?.name") String username, Model model, HttpServletRequest request) {
-        User user = userRepository.findByUsername(username);
-        Vendor vendor = vendorRepository.findVendorByUser(user);
+        User user = userService.findByUsername(username);
+        Vendor vendor = vendorUtility.findVendorByUser(user);
 
         request.getSession().setAttribute("user", user);
         request.getSession().setAttribute("vendor", vendor);
@@ -41,7 +41,7 @@ public class VendorController {
         return "vendors/individual_vendor";
     }
     @GetMapping("/vendors/categories/{id}")
-    public String vendorCategory(@PathVariable long id, Model model){
+    public String vendorCategory(@PathVariable long id, @CurrentSecurityContext(expression="authentication?.name") String username, Model model, HttpServletRequest request){
         model.addAttribute("id",id);
         VendorCategory vendorCategory = vendorCategoryService.findById(id);
         model.addAttribute("vendorCategory", vendorCategory);
