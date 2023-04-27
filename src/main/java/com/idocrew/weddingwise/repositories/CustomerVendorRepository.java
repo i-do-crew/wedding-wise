0f561dcd87aca3c3a60589f16bb9f5710bd9561b
Billie <billie.dorries28@gmail.com>
2023-04-26 19:43:13 -0500

    worked on liked vendor functionality

--- src/main/java/com/idocrew/weddingwise/repositories/CustomerVendorRepository.java
+++ src/main/java/com/idocrew/weddingwise/repositories/CustomerVendorRepository.java
@@ -2,10 +2,13 @@ package com.idocrew.weddingwise.repositories;
 
 import com.idocrew.weddingwise.entity.Customer;
 import com.idocrew.weddingwise.entity.CustomerVendor;
+import com.idocrew.weddingwise.entity.Vendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.Set;
 
 public interface CustomerVendorRepository extends JpaRepository<CustomerVendor, Long> {
     Set<CustomerVendor> findCustomerVendorByCustomer(Customer customer);
+    Optional<CustomerVendor> findCustomerVendorByCustomerAndVendor(Customer customer, Vendor vendor);
 }
