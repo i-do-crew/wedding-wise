16806a9b1621b13fa27559851affea9ea9d9ee7c
Clayton Priestley <clayton.priestley@gmail.com>
2023-04-28 14:08:50 -0500

    Fix 500 Error Due To Missing Session Variable - set 'vendor' session variable - also removed unnecessary Customer entities, but this was not impacting error

--- src/main/java/com/idocrew/weddingwise/controllers/VendorController.java
+++ src/main/java/com/idocrew/weddingwise/controllers/VendorController.java
@@ -1,10 +1,10 @@
 package com.idocrew.weddingwise.controllers;
 
 import com.idocrew.weddingwise.entity.*;
-import com.idocrew.weddingwise.repositories.UserRepository;
-import com.idocrew.weddingwise.repositories.VendorCategoryRepository;
-import com.idocrew.weddingwise.repositories.VendorRepository;
-import com.idocrew.weddingwise.services.*;
+import com.idocrew.weddingwise.services.CustomerVendorService;
+import com.idocrew.weddingwise.services.UserService;
+import com.idocrew.weddingwise.services.VendorCategoryService;
+import com.idocrew.weddingwise.services.VendorUtility;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,20 +24,19 @@ public class VendorController {
     private final VendorUtility vendorUtility;
     private final VendorCategoryService vendorCategoryService;
     private final UserService userService;
-    private final CustomerService customerService;
-    private final BudgetEntryService budgetEntryService;
     private final CustomerVendorService customerVendorService;
 
     private void refactorThisMethod(@CurrentSecurityContext(expression = "authentication?.name") String username, Model model, HttpServletRequest request) {
         User user = userService.findByUsername(username);
-        Customer customer = customerService.findCustomerByUser(user);
+        //Customer customer = customerService.findCustomerByUser(user);
         List<VendorCategory> vendorCategories = vendorCategoryService.findAll();
-        List<BudgetEntry> budget = budgetEntryService.findBudgetEntriesByCustomer(customer);
-        Set<CustomerVendor> customerVendors = customerVendorService.findByCustomer(customer);
+        //List<BudgetEntry> budget = budgetEntryService.findBudgetEntriesByCustomer(customer);
+        //Set<CustomerVendor> customerVendors = customerVendorService.findByCustomer(customer);
         request.getSession().setAttribute("user", user);
-        request.getSession().setAttribute("customer", customer);
-        request.getSession().setAttribute("budget", budget);
-        request.getSession().setAttribute("customerVendors", customerVendors);
+        request.getSession().setAttribute("vendor", user.getVendor());
+        //request.getSession().setAttribute("customer", customer);
+        //request.getSession().setAttribute("budget", budget);
+        //request.getSession().setAttribute("customerVendors", customerVendors);
         request.getSession().setAttribute("categories", vendorCategories);
     }
     @GetMapping("/vendors/individual/{id}")
