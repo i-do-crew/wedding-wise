a7bc1b25529ed0b89563ac1cdad8678bce954412
Clayton Priestley <clayton.priestley@gmail.com>
2023-04-28 20:23:07 -0500

    Speed Up Login Wait Time - Revised UserService, CustomerService and PrincipalGroup service to use native SQL queries

--- src/main/java/com/idocrew/weddingwise/services/impl/PrincipalGroupServiceImpl.java
+++ src/main/java/com/idocrew/weddingwise/services/impl/PrincipalGroupServiceImpl.java
@@ -1,15 +1,21 @@
 package com.idocrew.weddingwise.services.impl;
 
 import com.idocrew.weddingwise.entity.PrincipalGroup;
+import com.idocrew.weddingwise.entity.User;
 import com.idocrew.weddingwise.repositories.PrincipalGroupRepository;
 import com.idocrew.weddingwise.services.PrincipalGroupService;
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service("principalGroupService")
 class PrincipalGroupServiceImpl implements PrincipalGroupService {
     private final PrincipalGroupRepository principalGroupRepository;
+    private final EntityManager em;
+
     @Override
     public PrincipalGroup savePrincipalGroup(PrincipalGroup principalGroup) {
         return principalGroupRepository.save(principalGroup);
@@ -18,5 +24,12 @@ class PrincipalGroupServiceImpl implements PrincipalGroupService {
     @Override
     public PrincipalGroup findByCode(String role) {
         return principalGroupRepository.findByCode(role);
+    }
+
+    @Override
+    public Set<PrincipalGroup> findByUser(User user) {
+        String query = String.format("select g.* from principal_groups g where g.id=(select ug.group_id from user_groups ug where ug.user_id=%d);", user.getId());
+        PrincipalGroup group = (PrincipalGroup) em.createNativeQuery(query, PrincipalGroup.class).getSingleResult();
+        return Set.of(group);
     }
 }
--- src/main/java/com/idocrew/weddingwise/services/impl/UserDetailsImpl.java
+++ src/main/java/com/idocrew/weddingwise/services/impl/UserDetailsImpl.java
@@ -1,31 +1,36 @@
 package com.idocrew.weddingwise.services.impl;
 
+import com.idocrew.weddingwise.entity.PrincipalGroup;
 import com.idocrew.weddingwise.entity.User;
 import com.idocrew.weddingwise.entity.UserWithRoles;
-import com.idocrew.weddingwise.repositories.UserRepository;
+import com.idocrew.weddingwise.services.PrincipalGroupService;
+import com.idocrew.weddingwise.services.UserService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 public class UserDetailsImpl implements UserDetailsService {
-    private final UserRepository users;
+    private final UserService userService;
+    PrincipalGroupService principalGroupService;
 
-    public UserDetailsImpl(UserRepository users) {
-        this.users = users;
+    public UserDetailsImpl(UserService userService, PrincipalGroupService principalGroupService) {
+        this.userService = userService;
+        this.principalGroupService = principalGroupService;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = users.findByUsername(username);
 
-        if (user == null) {
-            throw new UsernameNotFoundException("No user found for " + username);
-        }
+        User user = userService.findByUsername(username);
+        Set<PrincipalGroup> groups = principalGroupService.findByUser(user);
+        user.setUserGroups(groups);
 
         //TODO: Uncomment once verification processing is complete
-        //user.setLoginDisabled(!user.isAccountVerified());
+        user.setLoginDisabled(!user.isAccountVerified());
 
         return new UserWithRoles(user);
     }
